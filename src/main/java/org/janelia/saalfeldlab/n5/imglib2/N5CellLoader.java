9bfaeac4c4db112c063d33a6ea46d8ac647e6c66
Igor Pisarev <pisarevi@janelia.hhmi.org>
2017-06-19 11:46:16 -0400

    fix NPE for sparse datasets where some blocks are not present

--- src/main/java/org/janelia/saalfeldlab/n5/imglib2/N5CellLoader.java
+++ src/main/java/org/janelia/saalfeldlab/n5/imglib2/N5CellLoader.java
@@ -54,7 +54,7 @@ public class N5CellLoader< T extends NativeType< T > > implements CellLoader< T 
 		final long[] gridPosition = new long[ interval.numDimensions() ];
 		for ( int d = 0; d < gridPosition.length; ++d )
 			gridPosition[ d ] = interval.min( d ) / cellDimensions[ d ];
-		DataBlock< ? > block;
+		final DataBlock< ? > block;
 		try
 		{
 			block = n5.readBlock( dataset, attributes, gridPosition );
@@ -64,7 +64,8 @@ public class N5CellLoader< T extends NativeType< T > > implements CellLoader< T 
 			throw new RuntimeException( e );
 		}
 
-		copyFromBlock.accept( interval, block );
+		if ( block != null )
+			copyFromBlock.accept( interval, block );
 	}
 
 	public static < T extends Type< T > > void burnIn( final RandomAccessibleInterval< T > source, final RandomAccessibleInterval< T > target )
