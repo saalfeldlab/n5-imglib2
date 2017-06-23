3acf947ccb3768b2f9ea5aa8aaca79a3137c8c2f
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2017-06-22 22:39:34 -0400

    added simple viewer test for bdv-vistools multi-scale mipmap source

--- src/main/java/org/janelia/saalfeldlab/n5/imglib2/Test.java
+++ src/main/java/org/janelia/saalfeldlab/n5/imglib2/Test.java
@@ -5,14 +5,18 @@ import java.util.Arrays;
 
 import org.janelia.saalfeldlab.n5.DatasetAttributes;
 import org.janelia.saalfeldlab.n5.N5;
+import org.janelia.saalfeldlab.n5.N5Reader;
 import org.janelia.saalfeldlab.n5.N5Writer;
 
 import bdv.util.BdvFunctions;
 import bdv.util.BdvOptions;
 import bdv.util.BdvStackSource;
+import bdv.util.RandomAccessibleIntervalMipmapSource;
 import bdv.util.volatiles.SharedQueue;
 import bdv.util.volatiles.VolatileViews;
 import bdv.viewer.DisplayMode;
+import mpicbg.spim.data.sequence.FinalVoxelDimensions;
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.RealRandomAccessible;
 import net.imglib2.cache.img.DiskCachedCellImg;
 import net.imglib2.cache.img.DiskCachedCellImgFactory;
@@ -22,14 +26,16 @@ import net.imglib2.interpolation.randomaccess.NLinearInterpolatorFactory;
 import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.realtransform.RealViews;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
 import net.imglib2.type.volatiles.VolatileUnsignedByteType;
+import net.imglib2.type.volatiles.VolatileUnsignedShortType;
 import net.imglib2.view.RandomAccessibleOnRealRandomAccessible;
 import net.imglib2.view.Views;
 
 public class Test
 {
 
-	public static void main( final String[] args ) throws IOException
+	public static void main1( final String[] args ) throws IOException
 	{
 		// davi
 //		final String n5path = "/data/hanslovskyp/n5-tests/davi-toy-set";
@@ -80,6 +86,39 @@ public class Test
 		final DiskCachedCellImg< UnsignedByteType, ? > transformedCellImg = factory.create( dim, new UnsignedByteType(), transformLoader );
 		BdvFunctions.show( VolatileViews.wrapAsVolatile( transformedCellImg, queue ), "transformed", BdvOptions.options().addTo( bdv ) );
 		bdv.getBdvHandle().getSetupAssignments().getMinMaxGroups().get( 1 ).setRange( 0, 255 );
+	}
+
+	public static void main( final String[] args ) throws IOException
+	{
+		// nrs
+		final String n5path = "/nrs/saalfeld/igor/161211_YFP2_comparison2/n5-export/c0";
+		final N5Reader n5 = N5.openFSReader( n5path );
+
+		@SuppressWarnings( "unchecked" )
+		final RandomAccessibleInterval< UnsignedShortType >[] mipmapSources = new RandomAccessibleInterval[6];
+		final int[][] mipmapScales = new int[ 6 ][ 3 ];
+
+		final int[] downsampleFactors = new int[]{ 1, 1, 2, 4, 9, 17 };
+
+		for ( int s = 0; s < 6; ++s )
+		{
+			mipmapSources[ s ] = N5Utils.openVolatile( n5, "s" + s );
+			mipmapScales[ s ][ 0 ] = mipmapScales[ s ][ 1 ] = 1 << s;
+			mipmapScales[ s ][ 2 ] = downsampleFactors[ s ];
+		}
+
+		final int numProc = Runtime.getRuntime().availableProcessors();
+		final SharedQueue queue = new SharedQueue(Math.max(1, numProc / 2));
+
+		final RandomAccessibleIntervalMipmapSource< UnsignedShortType > source =
+				new RandomAccessibleIntervalMipmapSource< UnsignedShortType >(
+						mipmapSources,
+						new UnsignedShortType(),
+						mipmapScales,
+						new FinalVoxelDimensions( "um", 0.097, 0.097, 0.18 ),
+						"buh" );
+
+		BdvFunctions.show( source.asVolatile( new VolatileUnsignedShortType(), queue ), BdvOptions.options() );
 	}
 
 }
