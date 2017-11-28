f01e3cd015313dd685a6fbb6b5d1b161d36ff3b1
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2017-11-28 10:22:44 -0500

    remove unused inner class from RandomAccessibleLoader (still in use?)
    TODO find out whether RandomAccessibleLoader is still needed

--- src/main/java/org/janelia/saalfeldlab/n5/imglib2/RandomAccessibleLoader.java
+++ src/main/java/org/janelia/saalfeldlab/n5/imglib2/RandomAccessibleLoader.java
@@ -1,27 +1,22 @@
 package org.janelia.saalfeldlab.n5.imglib2;
 
 import net.imglib2.Cursor;
-import net.imglib2.Interval;
 import net.imglib2.RandomAccessible;
 import net.imglib2.cache.img.CellLoader;
 import net.imglib2.cache.img.SingleCellArrayImg;
-import net.imglib2.img.array.ArrayImg;
 import net.imglib2.type.NativeType;
-import net.imglib2.util.Intervals;
 import net.imglib2.view.Views;
 
+/**
+ * A {@link CellLoader} that copies its data from a {@link RandomAccessible}.
+ *
+ * @param <T>
+ *
+ * @author Philipp Hanslovsky
+ * @author Stephan Saalfeld
+ */
 public class RandomAccessibleLoader< T extends NativeType< T > > implements CellLoader< T >
 {
-	public static interface AccessGenerator< T extends NativeType< T >, A >
-	{
-		public ArrayImg< T, A > createAccess( long[] dim );
-
-		default public ArrayImg< T, A > createAccess( final Interval interval )
-		{
-			return createAccess( Intervals.dimensionsAsLongArray( interval ) );
-		}
-	}
-
 	private final RandomAccessible< T > source;
 
 	public RandomAccessibleLoader( final RandomAccessible< T > source )
