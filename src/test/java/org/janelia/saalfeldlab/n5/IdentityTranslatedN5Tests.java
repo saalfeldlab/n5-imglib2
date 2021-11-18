300e4b2734ffb9c58fe357012224458b8ec6f5af
bogovicj <bogovicj@janelia.hhmi.org>
2021-11-18 10:11:30 -0500

    fix: translated identity test

--- src/test/java/org/janelia/saalfeldlab/n5/IdentityTranslatedN5Tests.java
+++ src/test/java/org/janelia/saalfeldlab/n5/IdentityTranslatedN5Tests.java
@@ -1,8 +1,16 @@
 package org.janelia.saalfeldlab.n5;
 
+import static org.junit.Assert.fail;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.janelia.saalfeldlab.n5.translation.TranslatedN5Writer;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.gson.reflect.TypeToken;
 
 public class IdentityTranslatedN5Tests extends AbstractN5Test {
 	
@@ -14,4 +22,52 @@ public class IdentityTranslatedN5Tests extends AbstractN5Test {
 		return n5;
 	}
 	
+	@Override
+	@Test
+	public void testAttributes() {
+
+		try {
+			n5.createGroup(groupName);
+
+			n5.setAttribute(groupName, "key1", "value1");
+			// path
+			Assert.assertEquals(2, n5.listAttributes(groupName).size());
+
+			/* class interface */
+			Assert.assertEquals("value1", n5.getAttribute(groupName, "key1", String.class));
+			/* type interface */
+			Assert.assertEquals("value1", n5.getAttribute(groupName, "key1", new TypeToken<String>(){}.getType()));
+
+			final Map<String, String> newAttributes = new HashMap<>();
+			newAttributes.put("key2", "value2");
+			newAttributes.put("key3", "value3");
+			n5.setAttributes(groupName, newAttributes);
+			Assert.assertEquals(4, n5.listAttributes(groupName).size());
+			/* class interface */
+			Assert.assertEquals("value1", n5.getAttribute(groupName, "key1", String.class));
+			Assert.assertEquals("value2", n5.getAttribute(groupName, "key2", String.class));
+			Assert.assertEquals("value3", n5.getAttribute(groupName, "key3", String.class));
+			/* type interface */
+			Assert.assertEquals("value1", n5.getAttribute(groupName, "key1", new TypeToken<String>(){}.getType()));
+			Assert.assertEquals("value2", n5.getAttribute(groupName, "key2", new TypeToken<String>(){}.getType()));
+			Assert.assertEquals("value3", n5.getAttribute(groupName, "key3", new TypeToken<String>(){}.getType()));
+
+			// test the case where the resulting file becomes shorter
+			n5.setAttribute(groupName, "key1", new Integer(1));
+			n5.setAttribute(groupName, "key2", new Integer(2));
+			Assert.assertEquals(4, n5.listAttributes(groupName).size());
+			/* class interface */
+			Assert.assertEquals(new Integer(1), n5.getAttribute(groupName, "key1", Integer.class));
+			Assert.assertEquals(new Integer(2), n5.getAttribute(groupName, "key2", Integer.class));
+			Assert.assertEquals("value3", n5.getAttribute(groupName, "key3", String.class));
+			/* type interface */
+			Assert.assertEquals(new Integer(1), n5.getAttribute(groupName, "key1", new TypeToken<Integer>(){}.getType()));
+			Assert.assertEquals(new Integer(2), n5.getAttribute(groupName, "key2", new TypeToken<Integer>(){}.getType()));
+			Assert.assertEquals("value3", n5.getAttribute(groupName, "key3", new TypeToken<String>(){}.getType()));
+
+		} catch (final IOException e) {
+			fail(e.getMessage());
+		}
+	}
+
 }
